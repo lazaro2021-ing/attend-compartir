6c4061c7bad0ad0d070ec0c71b4450773e3624ea
lazaro2021-ing <lazarodeusich@gmail.com>
2021-05-03 01:58:31 -0300

    test-30

--- app/present/routes.py
+++ app/present/routes.py
@@ -40,9 +40,9 @@ def generar():
     data_hash=str(hash(jsonify(data_req)))
     qr = qrcode.QRCode(version=1,border=0,box_size=8)
     #local
-    url=f'localhost:5000/present/confirmar?hash={data_hash}'
+    #url=f'localhost:5000/present/confirmar?hash={data_hash}'
     #heroku
-    #url=f'https://attendlzr.herokuapp.com/present/confirmar?hash={data_hash}'
+    url=f'https://attendlzr.herokuapp.com/present/confirmar?hash={data_hash}'
     qr.add_data(url)
     img=qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
